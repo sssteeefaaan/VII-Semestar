ba473d909f712a9f290c14fb13d39e276c578c4a
Stefan Aleksic <sssteeefaaan@elfak.rs>
2021-12-01 19:26:41 +0100

    update

--- Veštačka Inteligencija/Laboratorijske Vežbe/Lab 3/8.py
+++ Veštačka Inteligencija/Laboratorijske Vežbe/Lab 3/8.py
@@ -1,82 +1,126 @@
-# from functools import reduce
-# import operator
+from functools import reduce
+import operator
+import copy
+
+def pseudokuAStar(table):
+    open_set = set()
+    closed_set = set()
+    prev_state = {}
+    g = {}
+    h = {}
+    path = []
+
+    store = {'0': {}}
+    store['0']['table'] = table
+    store['0']['taken'] = set([x for y in table for x in y if x != 0])
+    store['0']['free'] = set(
+        [x for x in range(1, 10) if x not in store['0']['taken']])
+
+    end = False
+    open_set.add('0')
+    g['0'] = 0
+    h['0'] = heuritics(store['0'])
+    prev_state['0'] = None
+
+    while open_set and not end:
+        state = None
+        for next_state in open_set:
+            h[next_state] = heuritics(store[next_state])
+            if state is None or g[next_state] + h[next_state] < g[state] + h[state]:
+                state = next_state
+
+        if isComplete(store[state]):
+            end = True
+            break
+
+        for ns in findNext(store[state]):
+            k = str(len(store.keys()))
+            store |= {
+                k: ns
+            }
+            if k not in open_set and k not in closed_set:
+                open_set.add(k)
+                prev_state[k] = state
+                h[k] = heuritics(ns)
+                g[k] = g[state] + h[k]
+            elif g[k] > g[state] + h[k]:
+                g[k] = g[state] + h[k]
+                prev_state[k] = state
+                if k in closed_set:
+                    closed_set.remove(k)
+                    open_set.add(k)
+
+        open_set.remove(state)
+        closed_set.add(state)
+
+    if end:
+        prev = state
+        while prev_state[prev] is not None:
+            path = [store[prev]['table']] + path
+            prev = prev_state[prev]
+        path = [table] + path
+
+    return path
 
 
-# def findComb(state, i, j):
-#     table = state[0]
-#     free = state[1]
-#     r = [x[1] for x in table.items() if x[0][0] == i]
-#     if len(r)==3:
-#         combR
-#     c = [x[1] for x in table.items() if x[0][1] == j]
-#     sumC = reduce(operator.add, c, 0)
-#     print('Table')
-#     for indi in range(1, 4):
-#         for indj in range(1, 4):
-#             print(table.get(str(indi) + str(indj), "x"), end=" ")
-#         print(end="\n")
-#     print('Element indeks:', i, j)
-#     print('Row sum:', sumR)
-#     print('Column sum:', sumC)
-
-#     ret = []
-#     for el in free:
-#         if (len(r) < 2 and sumR + el < 15 or len(r) == 2 and sumR + el == 15 or len(r)==3) and (len(c) < 2 and sumC + el <= 15 or len(c) == 2 and sumC + el == 15 or len(c)==3):
-#             ret += [[table | {i + j: el}, set([x for x in free if x != el])]]
-#     return ret
+# To be implemented
+def heuritics(s):
+    return 0
 
 
-# def pseudoku(table):
-#     solutions = []
-#     taken = set(table.values())
-#     state = [[dict(table), set([x for x in range(1, 10) if x not in taken])]]
-#     while state:
-#         curr, *state = state
-#         if len(curr[0].keys()) == 9:
-#             return curr[0]
-#         for i in range(1, 4):
-#             for j in range(1, 4):
-#                 if not curr[0].get(str(i) + str(j), False):
-#                     state += findComb(curr, str(i), str(j))
-#     return solutions
+def findNext(state):
+    newStates = []
+    table = state['table']
+    taken = state['taken']
+    free = state['free']
+
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if table[i][j] == 0:
+                for el in free:
+                    cp = copy.deepcopy(table)
+                    cp[i][j]=el
+                    newStates += [{
+                        'table': cp,
+                        'taken': taken | set([el]),
+                        'free': set(filter(lambda a: a != el, free))
+                    }]
+
+    return list(filter(isValid, newStates))
 
 
-from itertools import permutations
+def isValid(state):
+    table = state['table']
+    free = state['free']
+    for r in [list(filter(lambda y: y != 0, x)) for x in table]:
+        rSum = reduce(operator.add, r, 0)
+        if len(r) == 3 and rSum != 15:
+            return False
+        elif len(r) == 2 and (15 - rSum) not in free:
+            return False
+    for c in [list(filter(lambda y: y != 0, x)) for x in list(zip(*table))]:
+        cSum = reduce(operator.add, c, 0)
+        if len(c) == 3 and cSum != 15:
+            return False
+        elif len(c) == 2 and (15 - cSum) not in free:
+            return False
+    return True
 
 
-def allSums():
+def isComplete(state):
+    table = state['table']
+    for r in table:
+        if reduce(operator.add, r, 0) != 15:
+            return False
+    for c in list(zip(*table)):
+        if reduce(operator.add, c, 0) != 15:
+            return False
+    return True
 
-    r = range(1, 10)
-    grids = permutations(r)
-
-    solutions = [grid for grid in grids if allSumsAre15(grid)]
-    return solutions
-
-
-def allSumsAre15(grid):
-    return (sumIs15(grid, 0, 1, 2) and
-            sumIs15(grid, 3, 4, 5) and
-            sumIs15(grid, 6, 7, 8) and
-            sumIs15(grid, 0, 3, 6) and
-            sumIs15(grid, 1, 4, 7) and
-            sumIs15(grid, 2, 5, 8))
-
-
-def sumIs15(grid, a, b, c):
-    sum_ = grid[a] + grid[b] + grid[c]
-    return sum_ == 15
-
-
-def printAll():
-    for array in allSums():
-        printArray(array)
-
-
-def printArray(array):
-    print("-----")
-    for i in range(0, 9, 3):
-        print(array[i], array[i+1], array[i+2])
-
-
-printAll()
-# print(pseudoku({'11': 1, '33': 9}))
+road2Success = pseudokuAStar([[1, 0, 0], [8, 0, 0], [0]*3])
+for step in road2Success:
+    for row in step:
+        for val in row:
+            print("\t", val, end=" ")
+        print()
+    print("-" * 40)
